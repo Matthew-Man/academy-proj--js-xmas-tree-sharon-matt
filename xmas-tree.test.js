0ca53a60ea5b850ccfe90d5c8f4db908e38519e3
Matthew Man <matthew08m@gmail.com>
2021-02-02 14:21:19 +0000

    new tests added

--- xmas-tree.test.js
+++ xmas-tree.test.js
@@ -9,11 +9,13 @@ const {
 test("findLineWidth finds the line width from a foliage height", () => {
   expect(findLineWidth(5)).toBe(9);
   expect(findLineWidth(3)).toBe(5);
+  expect(findLineWidth(4)).toBe(7)
 });
 
 test("makeTreeTrunk returns the tree trunk part of the Christmas tree with given foliage height", () => {
   expect(makeTreeTrunk(5)).toStrictEqual(["____#____", "____#____"]);
   expect(makeTreeTrunk(3)).toStrictEqual(["__#__", "__#__"]);
+  expect(makeTreeTrunk(4)).toStrictEqual(["___#___", "___#___"]);
 });
 
 test("makeFoliageSegment returns the foliage segment for a given level of a tree of given foliage height", () => {
@@ -32,6 +34,7 @@ test("makeTreeFoliage returns the foliage for a tree of given foliage height", (
     "#########",
   ]);
   expect(makeTreeFoliage(3)).toStrictEqual(["__#__", "_###_", "#####"]);
+  expect(makeTreeFoliage(4)).toStrictEqual(["___#___", "__###__", "_#####_","#######"]);
 });
 
 test("xmasTree returns an array modelling a Christmas tree of given foliage height", () => {
@@ -51,61 +54,70 @@ test("xmasTree returns an array modelling a Christmas tree of given foliage heig
     "__#__",
     "__#__",
   ]);
+  expect(xmasTree(4)).toStrictEqual([
+    "___#___", 
+    "__###__", 
+    "_#####_",
+    "#######",
+    "___#___",
+    "___#___",
+  ]);
 });
 
+// let array = [1, 2, 3]
 
-test.skip("toBe on arrays - experiment", () => {
-  expect([1, 2, 3]).toBe([1, 2, 3]);
-  expect(["a", "b", "c"]).toBe(["a", "b", "c"]);
-});
+// test("toBe on arrays - experiment", () => {
+//   expect(array).toBe(array);
+//   expect(["a", "b", "c"]).toBe(["a", "b", "c"]);
+// });
 
-test.skip("toStrictEqual on arrays - experiment", () => {
-  expect([1, 2, 3]).toStrictEqual([1, 2, 3]);
-  expect(["a", "b", "c"]).toStrictEqual(["a", "b", "c"]);
-});
+// test("toStrictEqual on arrays - experiment", () => {
+//   expect([1, 2, 3]).toStrictEqual([1, 2, 3]);
+//   expect(["a", "b", "c"]).toStrictEqual(["a", "b", "c"]);
+// });
 
-test.skip("toBe on booleans - experiment", () => {
-  expect(true).toBe(true);
-  expect(false).toBe(false);
-});
+// test("toBe on booleans - experiment", () => {
+//   expect(true).toBe(true);
+//   expect(false).toBe(false);
+// });
 
-test.skip("toStrictEqual on booleans - experiment", () => {
-  expect(true).toStrictEqual(true);
-  expect(false).toStrictEqual(false);
-});
+// test("toStrictEqual on booleans - experiment", () => {
+//   expect(true).toStrictEqual(true);
+//   expect(false).toStrictEqual(false);
+// });
 
-test.skip("toBe on numbers - experiment", () => {
-  expect(1).toBe(1);
-  expect(100).toBe(100);
-});
+// test("toBe on numbers - experiment", () => {
+//   expect(1).toBe(1);
+//   expect(100).toBe(100);
+// });
 
-test.skip("toStrictEqual on numbers - experiment", () => {
-  expect(1).toStrictEqual(1);
-  expect(100).toStrictEqual(100);
-});
+// test("toStrictEqual on numbers - experiment", () => {
+//   expect(1).toStrictEqual(1);
+//   expect(100).toStrictEqual(100);
+// });
 
-test.skip("toBe on objects - experiment", () => {
-  expect({ firstName: "Richard", lastName: "Ng" }).toBe({
-    firstName: "Richard",
-    lastName: "Ng",
-  });
-  expect({ a: "A", b: "B", c: "C" }).toBe({ a: "A", b: "B", c: "C" });
-});
+// test("toBe on objects - experiment", () => {
+//   expect({ firstName: "Richard", lastName: "Ng" }).toBe({
+//     firstName: "Richard",
+//     lastName: "Ng",
+//   });
+//   expect({ a: "A", b: "B", c: "C" }).toBe({ a: "A", b: "B", c: "C" });
+// });
 
-test.skip("toStrictEqual on objects - experiment", () => {
-  expect({ firstName: "Richard", lastName: "Ng" }).toStrictEqual({
-    firstName: "Richard",
-    lastName: "Ng",
-  });
-  expect({ a: "A", b: "B", c: "C" }).toStrictEqual({ a: "A", b: "B", c: "C" });
-});
+// test("toStrictEqual on objects - experiment", () => {
+//   expect({ firstName: "Richard", lastName: "Ng" }).toStrictEqual({
+//     firstName: "Richard",
+//     lastName: "Ng",
+//   });
+//   expect({ a: "A", b: "B", c: "C" }).toStrictEqual({ a: "A", b: "B", c: "C" });
+// });
 
-test.skip("toBe on strings - experiment", () => {
-  expect("hello").toBe("hello");
-  expect("world").toBe("world");
-});
+// test("toBe on strings - experiment", () => {
+//   expect("hello").toBe("hello");
+//   expect("world").toBe("world");
+// });
 
-test.skip("toStrictEqual on strings - experiment", () => {
-  expect("hello").toStrictEqual("hello");
-  expect("world").toStrictEqual("world");
-});
+// test("toStrictEqual on strings - experiment", () => {
+//   expect("hello").toStrictEqual("hello");
+//   expect("world").toStrictEqual("world");
+// });
